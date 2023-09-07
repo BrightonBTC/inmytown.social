057bff41a5455d5fb869cd9e2a452d70bf393e48
BrightonBTC <ccodling+bbtc@googlemail.com>
2023-09-07 16:01:53 +0100

    fix issue with duplicate listings

--- src/routes/CommunityListing.svelte
+++ src/routes/CommunityListing.svelte
@@ -3,18 +3,18 @@
     import CommunityCardLarge from '$lib/community/CommunityCardLarge.svelte';
     import { subCommunity, type CommunityMeta } from '$lib/community/community';
     import ndk from '$lib/ndk';
-    import { onMount } from 'svelte';
 
     export let community:string
 
     let communityDetails: CommunityMeta
 
-    onMount(async () => {
+    $: loadCommunity(), community;
 
+    async function loadCommunity() {
         subCommunity(ndk, community, async (data) => {
             communityDetails = data;
         });
-    })
+    }
 </script>
 <div class="mb-0 d-flex">
     {#if communityDetails}
--- src/routes/EventListing.svelte
+++ src/routes/EventListing.svelte
@@ -3,18 +3,18 @@
     import EventCard from '$lib/event/EventCard.svelte';
     import { subEventMeta, type EventMeta } from '$lib/event/event';
     import ndk from '$lib/ndk';
-    import { onMount } from 'svelte';
 
     export let event:string
 
     let eventData: EventMeta
 
-    onMount(async () => {
+    $: loadEvent(), event;
 
+    async function loadEvent() {
         subEventMeta(ndk, event, async (data) => {
             eventData = data;
         });
-    })
+    }
 </script>
 <div class="mb-0">
     
