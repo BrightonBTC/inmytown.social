c6f0e2d9de379981781907c5a2c41235a63d2179
BrightonBTC <ccodling+bbtc@googlemail.com>
2023-09-16 10:03:10 +0100

    tidy

--- src/routes/user/[npub]/+page.svelte
+++ src/routes/user/[npub]/+page.svelte
@@ -42,7 +42,6 @@
                     <a href="#t" class="list-group-item list-group-item-action" on:click|preventDefault={() => setPage('admin')}>Communities (admin)</a>
                     <a href="#t" class="list-group-item list-group-item-action" on:click|preventDefault={() => setPage('member')}>Communities (member)</a>
                     <a href="#t" class="list-group-item list-group-item-action" on:click|preventDefault={() => setPage('follows')}>Follows</a>
-                    <!-- <a href="#t" class="list-group-item list-group-item-action" on:click|preventDefault={() => setPage('del')}>DEL</a> -->
                 </div>
             </div>
             
@@ -56,8 +55,6 @@
             <Status {isLoggedInUser} />
             {:else if page==='follows'}
             <Follows />
-            {:else if page==='del'}
-            <!-- <Del {npub} /> -->
             {/if}
         </div>
     </div>
