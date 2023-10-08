50b2fb64ce2b3a3b6307cbd1392927270a86de05
BrightonBTC <ccodling+bbtc@googlemail.com>
2023-10-08 11:14:24 +0100

    refactor

--- src/lib/community/CommunityCard.svelte
+++ src/lib/community/CommunityCard.svelte
@@ -1,21 +1,27 @@
 <script lang="ts">
     import { imgUrlOrDefault } from "$lib/helpers";
+    import Location from "$lib/location/Location.svelte";
     import { Community, type CommunityMeta } from "./community";
 
     export let communityDetails: CommunityMeta;
 </script>
 <div class="card shadow">
-    <img
-        class="card-img-top header-image"
-        src={imgUrlOrDefault(communityDetails.image, 'community')}
-        alt={communityDetails.title}
-    />
+    <a href="{Community.url(communityDetails)}">
+        <img
+            class="card-img-top header-image"
+            src={imgUrlOrDefault(communityDetails.image, 'community')}
+            alt={communityDetails.title}
+        />
+    </a>
     <div class="card-body">
-        <h4 class="card-title">{communityDetails.title}</h4>
-        <a
-            href="{Community.url(communityDetails)}"
-            class="btn btn-primary float-end">Go to community</a
-        >
+        <h4 class="card-title mb-0">
+            <a class="text-decoration-none text-muted" href="{Community.url(communityDetails)}">
+                {communityDetails.title}
+            </a>
+        </h4>
+    </div>
+    <div class="card-footer small">
+        <Location country={communityDetails.country} city={communityDetails.city} />
     </div>
 </div>
 <style>
